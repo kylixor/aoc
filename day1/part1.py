eb9697d5cfe92ac393ecc8af3d873a6a959345e5
kylixor <93137659+kylixor@users.noreply.github.com>
2023-12-07 19:58:24 -0500

    Separated Part 1 and 2

--- day1/part1.py
+++ day1/part1.py
@@ -1,24 +1,9 @@
 file = open('day1/input.txt', 'r')
 lines = file.readlines()
 
-numlist = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-}
-
 sum = 0
 for line in lines:
-    fixed_line = line
-    for num, value in numlist.items():
-        fixed_line = fixed_line.replace(num, f'{num}{value}{num}')
-    digits = [x for x in fixed_line if x.isdigit()]
+    digits = [x for x in line if x.isdigit()]
     first_digit = digits[0]
     last_digit = digits[-1]
     number = int(first_digit + last_digit)
